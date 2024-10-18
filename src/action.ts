1a711cab84671bd2f9dd3ce3cd0fdb5c3ff01cca
XuluWarrior <github@metaliving.plus.com>
2024-10-18 18:35:35 +0100

    Only interested in @action related code

--- src/action.ts
+++ src/action.ts
@@ -1,52 +1,13 @@
+/**
+ * Derived from https://github.com/emberjs/ember.js/blob/main/packages/%40ember/object/index.ts
+ */
+
 import { assert } from '@ember/debug';
-import { ENV } from '@ember/-internals/environment';
 import type { ElementDescriptor, ExtendedMethodDecorator } from '@ember/-internals/metal';
 import {
     isElementDescriptor,
-    expandProperties,
     setClassicDecorator,
 } from '@ember/-internals/metal';
-import { getFactoryFor } from '@ember/-internals/container';
-import { setObservers } from '@ember/-internals/utils';
-import type { AnyFn } from '@ember/-internals/utility-types';
-import CoreObject from '@ember/object/core';
-import Observable from '@ember/object/observable';
-
-export {
-    notifyPropertyChange,
-    defineProperty,
-    get,
-    set,
-    getProperties,
-    setProperties,
-    computed,
-    trySet,
-} from '@ember/-internals/metal';
-
-/**
- @module @ember/object
- */
-
-/**
- `EmberObject` is the main base class for all Ember objects. It is a subclass
- of `CoreObject` with the `Observable` mixin applied. For details,
- see the documentation for each of these.
-
- @class EmberObject
- @extends CoreObject
- @uses Observable
- @public
- */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface EmberObject extends Observable {}
-class EmberObject extends CoreObject.extend(Observable) {
-    get _debugContainerKey() {
-        let factory = getFactoryFor(this);
-        return factory !== undefined && factory.fullName;
-    }
-}
-
-export default EmberObject;
 
 /**
  Decorator that turns the target function into an Action which can be accessed
@@ -226,86 +187,3 @@ export function action(
 
 // SAFETY: TS types are weird with decorators. This should work.
 setClassicDecorator(action as ExtendedMethodDecorator);
-
-// ..........................................................
-// OBSERVER HELPER
-//
-
-type ObserverDefinition<T extends AnyFn> = {
-    dependentKeys: string[];
-    fn: T;
-    sync: boolean;
-};
-
-/**
- Specify a method that observes property changes.
-
- ```javascript
- import EmberObject from '@ember/object';
- import { observer } from '@ember/object';
-
- export default EmberObject.extend({
- valueObserver: observer('value', function() {
- // Executes whenever the "value" property changes
- })
- });
- ```
-
- Also available as `Function.prototype.observes` if prototype extensions are
- enabled.
-
- @method observer
- @for @ember/object
- @param {String} propertyNames*
- @param {Function} func
- @return func
- @public
- @static
- */
-export function observer<T extends AnyFn>(
-    ...args:
-        | [propertyName: string, ...additionalPropertyNames: string[], func: T]
-        | [ObserverDefinition<T>]
-): T {
-    let funcOrDef = args.pop();
-
-    assert(
-        'observer must be provided a function or an observer definition',
-        typeof funcOrDef === 'function' || (typeof funcOrDef === 'object' && funcOrDef !== null)
-    );
-
-    let func: T;
-    let dependentKeys: string[];
-    let sync: boolean;
-
-    if (typeof funcOrDef === 'function') {
-        func = funcOrDef;
-        dependentKeys = args as string[];
-        sync = !ENV._DEFAULT_ASYNC_OBSERVERS;
-    } else {
-        func = funcOrDef.fn;
-        dependentKeys = funcOrDef.dependentKeys;
-        sync = funcOrDef.sync;
-    }
-
-    assert('observer called without a function', typeof func === 'function');
-    assert(
-        'observer called without valid path',
-        Array.isArray(dependentKeys) &&
-        dependentKeys.length > 0 &&
-        dependentKeys.every((p) => typeof p === 'string' && Boolean(p.length))
-    );
-    assert('observer called without sync', typeof sync === 'boolean');
-
-    let paths: string[] = [];
-
-    for (let dependentKey of dependentKeys) {
-        expandProperties(dependentKey, (path: string) => paths.push(path));
-    }
-
-    setObservers(func as Function, {
-        paths,
-        sync,
-    });
-    return func;
-}
