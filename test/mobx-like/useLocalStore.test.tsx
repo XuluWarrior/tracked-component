36c1d6db5864ca71558bffa886dd9844495d05cd
XuluWarrior <github@metaliving.plus.com>
2024-11-27 16:33:47 +0000

    fixup! Enforcing actions isn't relevant to Glimmer tracking

--- test/mobx-like/useLocalStore.test.tsx
+++ test/mobx-like/useLocalStore.test.tsx
@@ -1,6 +1,6 @@
 // import * as mobx from "mobx"
 import * as React from "react"
-import { renderHook } from "@testing-library/react-hooks"
+// import { renderHook } from "@testing-library/react-hooks"
 import { act, cleanup, fireEvent, render } from "@testing-library/react"
 
 import {useLocalTrackable, useTracking, trackedComponent, Tracking} from '../../src/index'
