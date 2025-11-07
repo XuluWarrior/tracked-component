f6351b7272856428d9b34678db2399472aa8f68d
XuluWarrior <github@metaliving.plus.com>
2025-11-07 23:50:54 +0000

    trackable is actually more like trackItems than trackedRecord But it also allows functions inside the tracked container to work as expected. This really doesn't fit with the `tracked` conceptual model.  So this really just a hack to get the mobx tests to work with minimal changes.

--- src/trackable.ts
+++ src/trackable.ts
@@ -1,10 +1,34 @@
-import  { trackedRecord } from "@xuluwarrior/tracked";
+import  { consumed, dirtyProp } from "@xuluwarrior/tracked";
 import { useState } from "react";
 
-export const trackable = trackedRecord
+
+export function trackable<T extends Record<string|symbol|number, any>>(a: T = {} as T): T {
+    const trackingSymbols: Record<symbol|string|number, symbol> = {}
+    function ensureKey(propertyKey: string | symbol) {
+        if (!trackingSymbols[propertyKey]) {
+            trackingSymbols[propertyKey] = Symbol(`tracked-${propertyKey.toString()}`)
+        }
+        return trackingSymbols[propertyKey];
+    }
+
+    const trackableHandler = {
+        get(target: T, propertyKey: string | symbol, receiver?: unknown) {
+
+            consumed(ensureKey(propertyKey))
+            const value =  Reflect.get(target, propertyKey, receiver);
+            return typeof value === "function" ? (value as Function).bind(receiver) : value
+        },
+        set(target: T, propertyKey: string | symbol, value: any, receiver?: unknown) {
+            dirtyProp(ensureKey(propertyKey));
+            return Reflect.set(target, propertyKey, value, receiver);
+        }
+    };
+    return new Proxy(a, trackableHandler);
+}
 
 export function useLocalTrackable<TStore extends Record<string, any>>(
     initializer: () => TStore,
 ): TStore {
-    return useState(() => trackable(initializer()))[0]
+    const [state] = useState(trackable(initializer()))
+    return state;
 }
